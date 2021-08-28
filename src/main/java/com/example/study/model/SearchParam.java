af47462ffc171a8816b66ace3b662092ee6b3b76
sion <aabbcc030421@gmail.com>
2021-08-28 11:23:57 +0900

    lombok 추가 & jpa 설정

--- src/main/java/com/example/study/model/SearchParam.java
+++ src/main/java/com/example/study/model/SearchParam.java
@@ -1,6 +1,10 @@
 package com.example.study.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
 
+@Data
+@AllArgsConstructor //모든 argument 포함한 생성자 만들기
 public class SearchParam {
 
     private String account;
